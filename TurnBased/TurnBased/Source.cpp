aef7e42a520f18c2ed4d9c9820db769c43cbcbfb
Tj Martinez <Tj_Martinez01@hotmail.com>
2018-08-06 21:40:02 -0600

    Figuring out JRPG type logic

--- TurnBased/TurnBased/Source.cpp
+++ TurnBased/TurnBased/Source.cpp
@@ -2,9 +2,33 @@
 using namespace std;
 #include <ctime>
 #include <string>
+class MONSTER {
+public:
+	void INITMONSTER(int hp, string name, int atk, int def, int spd);
+	bool DEAD(bool live);
+	int EXPRETURN(int exp);
+	void CREATEMONSTER(int randhp, string randname, int randatk, int randdef, int randspd);
+	;
+private:
+	int HP;
+	string NAME;
+	int ATK;
+	int DEF;
+	int SPD;
+	bool LIVE;
+	int EXP;
+
+	;
+};
 void SummonMonster(int randnum);
 void Help();
 int main() {
+	int PlayerHP = 5000;
+	string PlayerNAME = "Hero";
+	int PlayerATK = 2000;
+	int PlayerDEF = 2000;
+	int PlayerSPD = 3000;
+	int PlayerEXP = 0;
 	string action;
 	cout << "Welcome to Battle Training!" << endl;
 	cout << "This will cover Turn-based Text battle systems." << endl;
@@ -24,4 +48,21 @@ void Help() {
 	cout << "Use Defend to reduce damage." << endl;
 	cout << "Use Scan to see enemy statistics." << endl;
 	cout << "Use an item's name to use item." << endl;
+};
+
+void MONSTER::INITMONSTER(int hp, string name, int atk, int def, int spd) {
+	hp == HP;
+	name == NAME;
+	atk == ATK;
+	def == DEF;
+	spd == SPD;
+};
+bool MONSTER::DEAD(bool live) {
+	live == LIVE;
+};
+int MONSTER::EXPRETURN(int exp) {
+	exp == EXP
+};
+void MONSTER::CREATEMONSTER(int randhp, string randname, int randatk, int randdef, int randspd) {
+
 };
