056c6bd97400cc618202b28c27e1be0425617669
Tj Martinez <Tj_Martinez01@hotmail.com>
2018-08-06 21:23:04 -0600

    Figuring out JRPG type logic and such

---
+++ TurnBased/TurnBased/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <ctime>
+#include <string>
+void SummonMonster(int randnum);
+void Help();
+int main() {
+	string action;
+	cout << "Welcome to Battle Training!" << endl;
+	cout << "This will cover Turn-based Text battle systems." << endl;
+	cout << "Be prepared to start!" << endl;
+	cout << "Type \"Help\" to see the command list." << endl;
+	
+
+
+}
+void SummonMonster(int randnum) {
+
+};
+void Help() {
+	cout << "Use Help to view Help menu." << endl;
+	cout << "Use Item to view inventory." << endl;
+	cout << "Use Attack to damage enemy." << endl;
+	cout << "Use Defend to reduce damage." << endl;
+	cout << "Use Scan to see enemy statistics." << endl;
+	cout << "Use an item's name to use item." << endl;
+};
